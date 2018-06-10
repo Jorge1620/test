3e45e5e272a60b4d077b3cfddf3c16a5f34c9abc
Jorge Fuentes <jorge.fuentes@cartoguasa.local>
2018-06-10 00:53:30 -0600

    rutas angular

---
+++ src/app/app-routes.ts
@@ -0,0 +1,6 @@
+import {RouterModule, Routes } from '@angular/router'
+
+/*export const appRoutes = [
+	{path: "",  component: "HomeComponent"}
+]
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
 //componentes
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+
+//modulos
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './body/home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: "/home", pathMatch: "full" },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: FooterComponent },
+  { path: '**', redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
